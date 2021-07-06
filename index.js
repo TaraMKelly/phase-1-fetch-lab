393b4dee636cab830d933f91cb96174b3d57e54f
TaraMKelly <tara.kelly16@gmail.com>
2021-07-06 17:01:17 -0400

    commit tree 75c60e6ab51183522808f92b1bde123e20f3e570

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    return renderBooks(data)
+  }) 
 }
 
 function renderBooks(books) {
